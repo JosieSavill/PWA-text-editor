4bfd29cd5a06356b576e29eb5229774dde62de18
JosieSavill <fiordasavilljosie@gmail.com>
2023-05-09 00:30:41 -0700

    added asset caching to src-sw.js file

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,14 +1,14 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// Added an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
     deferredPrompt = event;
     butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+// Implemented a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     if (deferredPrompt) {
         const choiceResult = await deferredPrompt.userChoice;
@@ -17,7 +17,7 @@ butInstall.addEventListener('click', async () => {
     }
 });
 
-// TODO: Add an handler for the `appinstalled` event
+//  Added an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     console.log('Jate has been installed.');
 });
