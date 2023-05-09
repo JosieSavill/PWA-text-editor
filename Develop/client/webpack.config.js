c0d5702245849a8f6a318aec695b566ca1acf2c6
JosieSavill <fiordasavilljosie@gmail.com>
2023-05-09 00:57:33 -0700

    added workbox plugins for service worker manifest file, and added CSS loaders and babel to webpack

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,11 +18,54 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './serc/index.html',
+        filename: 'index.html',
+        chunks: ['main']
+    }),
+      new HtmlWebpackPlugin({
+        template: './src/install.html',
+        filename: 'install.html',
+        chunks: ['install']
+      }),
+      new WebpackPwaManifest({
+        name: 'My PWA',
+        short_name: 'My PWA',
+        description: 'This is my Progressive Web App!',
+        start_url: '.',
+        background_color: '#ffffff',
+        theme_color: '#2196f3',
+        icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [ 96, 128, 192, 256, 384, 512]
+          }
+        ]
+      }),
+
+      new InjectManifest({
+       swSrc: './src-sw.js',
+       swDest: 'sw.js' 
+      })
       
     ],
 
     module: {
       rules: [
+        {
+          test: /\.css#/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.mjs$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [ '@babel/preset-env']
+            }
+          }
+        }
         
       ],
     },
